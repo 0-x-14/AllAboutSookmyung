13461de8ae2b121a04b9deed407680f0805b70bd
choubung <choubung@sookmyung.ac.kr>
2022-10-10 12:48:25 +0900

    하단 탭/메인페이지 껍데기 레이아웃

--- app/src/main/java/com/cookandroid/all_about_sookmyung/MainActivity.java
+++ app/src/main/java/com/cookandroid/all_about_sookmyung/MainActivity.java
@@ -1,15 +1,67 @@
 package com.cookandroid.all_about_sookmyung;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    final String TAG = this.getClass().getSimpleName();
+
+    LinearLayout home_ly;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // test
+        Log.d(TAG, "");
+
+        init();
+        SettingListener();
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
+    }
+
+    private void init() {
+        home_ly = findViewById(R.id.home_ly);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+    }
+
+    private void SettingListener() {
+        // 선택 리스너 등록
+        bottomNavigationView.setOnNavigationItemSelectedListener(new TabSelectedListener());
+    }
+
+    class TabSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.tab_map: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.home_ly, new MapFragment())
+                            .commit();
+                    return true;
+                }
+                case R.id.tab_home: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.home_ly, new HomeFragment())
+                            .commit();
+                    return true;
+                }
+                case R.id.tab_menu: {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.home_ly, new MenuFragment())
+                            .commit();
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
