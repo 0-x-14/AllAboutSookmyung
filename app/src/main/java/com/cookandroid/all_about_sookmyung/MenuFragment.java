16d57a72720c947250da51b530b03a8169799422
choubung <choubung@sookmyung.ac.kr>
2022-11-08 00:48:02 +0900

    기타페이지 마이페이지 spinner 껍데기 구현

--- app/src/main/java/com/cookandroid/all_about_sookmyung/MenuFragment.java
+++ app/src/main/java/com/cookandroid/all_about_sookmyung/MenuFragment.java
@@ -7,6 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +28,12 @@ public class MenuFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private ArrayAdapter<String> adapter;
+
+    private Spinner spinner;
+    TextView student_room, department_office, locker;
+    String[] student_room_list = {};
+    String[] department_office_list = {};
 
     public MenuFragment() {
         // Required empty public constructor
@@ -58,7 +69,32 @@ public class MenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_menu, container, false);
+
+        student_room = (TextView) view.findViewById(R.id.studentRoom);
+        department_office = (TextView) view.findViewById(R.id.departmentOffice);
+        locker = (TextView) view.findViewById(R.id.locker);
+
+        spinner = (Spinner) view.findViewById(R.id.spinner);
+
+        ArrayAdapter departmentAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.department_spinner_array, android.R.layout.simple_spinner_dropdown_item);
+        departmentAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(departmentAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                student_room.setText(student_room_list[position]);
+                department_office.setText(department_office_list[position]);
+                Toast.makeText(getActivity(),"선택된 학과 : "+spinner.getItemAtPosition(position),Toast.LENGTH_SHORT).show();
+            } //이 오버라이드 메소드에서 position은 몇번째 값이 클릭됬는지 알 수 있습니다.
+            //getItemAtPosition(position)를 통해서 해당 값을 받아올수있습니다.
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) { }
+        });
+
+        return view;
     }
 }
