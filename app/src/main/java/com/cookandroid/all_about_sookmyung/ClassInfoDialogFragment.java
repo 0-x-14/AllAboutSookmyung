f76ed60ec89e79a4f528e964c5a9515b475c6392
choubung <choubung@sookmyung.ac.kr>
2022-12-15 18:29:07 +0900

    대화상자 최종

--- app/src/main/java/com/cookandroid/all_about_sookmyung/ClassInfoDialogFragment.java
+++ app/src/main/java/com/cookandroid/all_about_sookmyung/ClassInfoDialogFragment.java
@@ -2,34 +2,36 @@ package com.cookandroid.all_about_sookmyung;
 
 import android.app.*;
 import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.*;
 import android.widget.*;
 import androidx.annotation.*;
 
 public class ClassInfoDialogFragment extends androidx.fragment.app.DialogFragment {
-    String roomName; // 강의실 호수 또는 이름
+    String roomName, doorString; // 강의실 호수 또는 이름
     int roomKind; // 강의실 형태
-    boolean backdoorExist, plugExist; // 뒷문 콘센트 존재 유무
+    boolean plugExist; // 뒷문 콘센트 존재 유무
 
-    public ClassInfoDialogFragment (String roomName, boolean backdoorExist, boolean plugExist, int roomKind) {
+    TextView roomNumberTextView, backdoorTextview, plugTextView, roomKindTextView;
+    ImageView backdoorImageView, plugImageView, roomKindImageView;
+
+    public ClassInfoDialogFragment (String roomName, String doorString, boolean plugExist, int roomKind) {
         this.roomName = roomName;
-        this.backdoorExist = backdoorExist;
+        this.doorString = doorString;
         this.plugExist = plugExist;
         this.roomKind = roomKind;
     }
 
-    public static ClassInfoDialogFragment getInstance(String roomName, boolean backdoorExist, boolean plugExist, int roomKind) {
-        ClassInfoDialogFragment e = new ClassInfoDialogFragment(roomName, backdoorExist, plugExist, roomKind);
+    public static ClassInfoDialogFragment getInstance(String roomName, String doorString, boolean plugExist, int roomKind) {
+        ClassInfoDialogFragment e = new ClassInfoDialogFragment(roomName, doorString, plugExist, roomKind);
+        e.setStyle(STYLE_NO_TITLE, R.style.NewDialog);
         return e;
     }
 
-    TextView roomNumberTextView, backdoorTextview, plugTextView, roomKindTextView;
-    ImageView backdoorImageView, plugImageView, roomKindImageView;
     @Override
-    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        LayoutInflater inflater = getActivity().getLayoutInflater();
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.classroom_information, null);
 
         roomNumberTextView = (TextView) view.findViewById(R.id.roomNumber); // 텍스트들 가져오기
@@ -43,37 +45,37 @@ public class ClassInfoDialogFragment extends androidx.fragment.app.DialogFragmen
 
         roomNumberTextView.setText(roomName); // 강의실 이름 설정
 
-        // 문 설정
-        if (backdoorExist) {
-            backdoorTextview.setText("문이 여러 개예요");
-            backdoorImageView.setImageResource(R.drawable.ic_classroom_backdoor_existed);
-        } else {
-            backdoorTextview.setText("문이 하나예요");
+
+        if (doorString.equals("앞문") || doorString.equals("뒷문") || doorString.equals("중문")){
             backdoorImageView.setImageResource(R.drawable.ic_classroom_backdoor_absence);
+            backdoorTextview.setText(doorString+"만 있음");
+        } else {
+            backdoorImageView.setImageResource(R.drawable.ic_classroom_backdoor_existed);
+            backdoorTextview.setText(doorString);
         }
 
         // 플러그 유무
         if (plugExist){
-            plugTextView.setText("콘센트가 있어요");
+            plugTextView.setText("콘센트 있음");
             plugImageView.setImageResource(R.drawable.ic_classroom_plug_existed);
         } else {
-            plugTextView.setText("콘센트가 없어요");
+            plugTextView.setText("콘센트 없음");
             plugImageView.setImageResource(R.drawable.ic_classroom_plug_absence);
         }
 
         // 강의실 형태
         if (roomKind == 0) { // 교실형
-            roomKindTextView.setText("일반책상강의실이에요");
+            roomKindTextView.setText("교실형강의실");
             roomKindImageView.setImageResource(R.drawable.ic_classroom_table);
         } else if (roomKind == 1) { // 계단형
-            roomKindTextView.setText("계단강의실이에요");
+            roomKindTextView.setText("계단강의실");
             roomKindImageView.setImageResource(R.drawable.ic_classroom_stair);
         } else if (roomKind == 2) { // 조별책상형
-            roomKindTextView.setText("조별책상강의실이에요");
+            roomKindTextView.setText("조별책상강의실");
             roomKindImageView.setImageResource(R.drawable.ic_classroom_grouptable);
         }
 
-        return builder.create();
+        return view;
     }
 }
 
--- app/src/main/java/com/cookandroid/all_about_sookmyung/MapMyungsinFloor2Activity.java
+++ app/src/main/java/com/cookandroid/all_about_sookmyung/MapMyungsinFloor2Activity.java
@@ -1,5 +1,7 @@
 package com.cookandroid.all_about_sookmyung;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +19,7 @@ public class MapMyungsinFloor2Activity extends AppCompatActivity {
         r204Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ClassInfoDialogFragment room204Dialog = ClassInfoDialogFragment.getInstance("204호", true, false, 0);
+                ClassInfoDialogFragment room204Dialog = ClassInfoDialogFragment.getInstance("204호", "앞문", true, 0);
                 room204Dialog.show(getSupportFragmentManager(), "204호");
             }
         });
